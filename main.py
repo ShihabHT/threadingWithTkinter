a8c5493ab40e0116d509708d19e0af45f0fa9aff
ShihabHT <shihabrahman122@gmail.com>
2021-11-12 20:33:36 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import time
+import threading
 
 root = tk.Tk()
 
@@ -14,14 +15,28 @@ def change_egg():
     time.sleep(5)
     print("Conversion started, Please wait...")
     val = label.cget('text')
-    if val == "Ostrich Egg":
+    if val == 'Ostrich Egg':
         label.config(text="Snake egg")
     elif val == "Snake egg":
         label.config(text='Ostrich Egg')
 
 
-button1 = tk.Button(root, text="Convert Egg", command=change_egg)
+button1 = tk.Button(root, text="Convert Egg", command=threading.Thread(target=change_egg).start())
 button1.pack()
 
-button2 = tk.Button(root, )
+
+def make_decision():
+    print("Changing decision, Please wait...")
+    val = label1.cget('text')
+    if val == "But Chicken came first":
+        label1.config(text="But egg came first")
+    elif val == "But egg came first":
+        label1.config(text='But Chicken came first')
+
+
+label1 = tk.Label(root, text="But Chicken came first", bg="red")
+label1.pack()
+button2 = tk.Button(root, text='DECIDE', command=threading.Thread(target=make_decision).start())
+button2.pack()
+
 root.mainloop()
